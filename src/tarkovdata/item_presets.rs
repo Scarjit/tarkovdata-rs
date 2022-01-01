91f93f8b3b1a5ef7d6a83ad571dfc63bf18becdf
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2022-01-01 21:56:10 +0100

    feat: Added item resolver for item_presets

--- src/tarkovdata/item_presets.rs
+++ src/tarkovdata/item_presets.rs
@@ -1,11 +1,12 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 use std::collections::HashMap;
 
 
 pub type ItemPresets = HashMap<String, ItemPresetsValue>;
 
-use crate::tarkovdata::REPO_DIR;
+use crate::tarkovdata::{items_en, REPO_DIR};
 use cached::proc_macro::once;
+use crate::tarkovdata::items_en::ItemsEnValue;
 
 #[once]
 pub(crate) fn from_json() -> ItemPresets {
@@ -23,12 +24,31 @@ pub struct ItemPresetsValue {
     #[serde(rename = "default")]
     pub(crate) item_presets_default: bool,
     #[serde(rename = "baseId")]
-    pub(crate) base_id: String,
+    #[serde(deserialize_with = "items_from_number")]
+    pub(crate) base_id: ItemsEnValue,
     pub(crate) parts: Vec<Part>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Part {
-    pub(crate) id: String,
+    #[serde(deserialize_with = "items_from_number")]
+    pub(crate) id: ItemsEnValue,
     pub(crate) quantity: i64,
+}
+
+
+fn items_from_number<'de, D>(deserializer: D) -> Result<ItemsEnValue, D::Error>
+    where
+        D: Deserializer<'de>,
+{
+    let n: &str = Deserialize::deserialize(deserializer)?;
+
+    let items = items_en::from_json();
+    Ok(items
+        .iter()
+        .filter(|t| t.1.id == n)
+        .next()
+        .unwrap()
+        .1
+        .clone())
 }
